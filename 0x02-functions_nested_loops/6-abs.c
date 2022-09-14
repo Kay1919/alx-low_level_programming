2423741eb7ea7ef43a97ef53bd38cdbe39a7bec1
Obinna <edwardmichael6.com>
2022-09-14 15:58:02 -0700

    test6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,7 +5,7 @@
  *   * @r: An integre input
  *    * Description: This function returns absolute value of a number
  *     * Return: Absolut value of number r
- *      */
+ **/
 int _abs(int r)
 {
 	if (r >= 0)
