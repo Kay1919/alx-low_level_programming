eb38ac8fac9feb96204cbaca70d2e733b6e36b5d
Obinna <edwardmichael6.com>
2022-09-13 19:35:25 -0700

    grep

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  *  * main - Check description
  *   * Description: It prints the word Holberton, followed by a new line.
  *    * Return: 0.
- *     */
+ **/
 int main(void)
 {
 	char word[9] = "Holberton";
