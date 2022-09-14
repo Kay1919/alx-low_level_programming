8838eeb0b709fc092c79ca53183f7967245cc9b7
Obinna <edwardmichael6.com>
2022-09-13 21:23:05 -0700

    pui

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,6 @@
 #include "main.h"
 
-/**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- **/
-
-int main(void)
+void print_alphabet(void)
 {
-	    print_alphabet();
-	        return (0);
+
 }
