6c070149aef4cec7e44c42f9741587eb896a6630
Obinna <edwardmichael6.com>
2022-09-14 17:19:45 -0700

    task11

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,8 +6,8 @@
  *    * @b: An integer b
  *     * Description: Function that adds two integers and returns the result
  *      * Return: Result to add a and b
- *       */
+ **/
 int add(int a, int b)
 {
-		return (a + b);
+	return (a + b);
 }
