12da60bfa571325fe16edf1822a51361e0bbf5c0
Obinna <edwardmichael6.com>
2022-09-14 17:46:52 -0700

    test9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,16 +18,12 @@ void times_table(void)
 			if ((n / 10) == 0)
 			{
 				if (j != 0)
-				{
 					_putchar(' ');
 					_putchar(n + '0');
-				}
 				if (j == 9)
-				{
 					continue;
 				_putchar(',');
 				_putchar(' ');
-				}
 			}
 			else
 			{
