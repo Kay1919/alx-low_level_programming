af2ce3b53c100e7a50bcb03b3fcc32638d15e4e2
Obinna <edwardmichael6.com>
2022-09-19 21:00:42 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - prints a string followed by a line..
- * by a new line, to stdout.
- * @str: an input string
- * Return: Nothing
- */
-
+ *  * _puts - Write a function that prints a string, followed
+ *   * by a new line, to stdout.
+ *    * @str: an input string
+ *     * Return: Nothing
+ **/
 void _puts(char *str)
 {
+	int i = 0;
 
-	int num;
-
-	for (num = 0; str[num] != '\0'; num++)
+	while (str[i] != '\0')
 	{
-		_putchar(str[num]);
+		_putchar(str[i]);
+			i++;
 	}
 	_putchar('\n');
 }
