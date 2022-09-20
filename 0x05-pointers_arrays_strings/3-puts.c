f2c1e22ed7e6c0243b5d5052564644d0a76497d4
Obinna <edwardmichael6.com>
2022-09-19 20:41:14 -0700

    main

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _puts - Write a function that prints a string, followed
+ * _puts - prints a string followed by a line..
  * by a new line, to stdout.
  * @str: an input string
  * Return: Nothing
