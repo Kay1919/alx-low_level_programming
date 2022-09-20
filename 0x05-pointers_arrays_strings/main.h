ee534544a6c2ae7fe50481a6a0b50e5fab20cdbd
Obinna <edwardmichael6.com>
2022-09-19 20:00:39 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,20 +1,8 @@
-#ifndef HOiLBERTON_H_INCLUDED
-#define HOiLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
 void reset_o_98(int *n)
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-void num(int r, int n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif
