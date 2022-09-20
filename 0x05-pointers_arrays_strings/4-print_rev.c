0741f69abe65f60c149baa71d49b2b192e3b6875
Obinna <edwardmichael6.com>
2022-09-19 21:20:12 -0700

    ret

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,16 +5,19 @@
  *   * followed by a new line.
  *    * @s: an input string
  *     * Return: Nothing
- *      */
+ **/
 void print_rev(char *s)
 {
 	int len = 0;
 
 	while (s[len] != '\0')
+	{
 		len++;
+	}
 
 	while (len)
+	{
 		putchar(s[--len]);
-
+	}
 	putchar('\n');
 }
