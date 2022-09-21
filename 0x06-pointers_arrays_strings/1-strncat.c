099ce4f03ac7204bcb1eb599ee4f806555fb92a2
Obinna <edwardmichael6.com>
2022-09-21 10:28:57 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  *  * _strncat - a function that concatenates two strings.
@@ -6,7 +6,7 @@
  *    * @src: an input string
  *     * @n: an input integer
  *      * Return: A pointer to the resulting string
- *       */
+ **/
 char *_strncat(char *dest, char *src, int n)
 {
 	int srclen = 0, i = 0;
