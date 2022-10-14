31c40d52f7baeb5a537791d5b0f4f86926220604
Obinna <edwardmichael6.com>
2022-10-13 21:06:45 -0700

    task0

---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  *void prototypes()
+  *int prototypes()
+ */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+
+int main(void);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif 
